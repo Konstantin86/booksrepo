33d1aa39ec5277618ab6d03c6d8a121ac4b747ee
Kostiantyn Lazurenko <kostyan22@gmail.com>
2025-02-20 13:04:43 +0200

    Added a test book record

--- GitDemoApi/Data/BookRepository.cs
+++ GitDemoApi/Data/BookRepository.cs
@@ -8,7 +8,8 @@ namespace GitDemoApi.Data
         public static List<Book> Books { get; set; } = new List<Book>
         {
             new Book { Id = 1, Title = "1984", Author = "George Orwell", Year = 1949 },
-            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960 }
+            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960 },
+            new Book { Id = 2, Title = "THE HOBBIT", Author = "J. R. R. Tolkien", Year = 1937 }
         };
     }
 }
