d4157a86a86189387a440fc8adbb0c0e7006a3eb
Kostiantyn Lazurenko <kostyan22@gmail.com>
2025-02-20 13:11:56 +0200

    added new test book records

--- GitDemoApi/Data/BookRepository.cs
+++ GitDemoApi/Data/BookRepository.cs
@@ -9,7 +9,14 @@ namespace GitDemoApi.Data
         {
             new Book { Id = 1, Title = "1984", Author = "George Orwell", Year = 1949 },
             new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960 },
-            new Book { Id = 2, Title = "THE HOBBIT", Author = "J. R. R. Tolkien", Year = 1937 }
+            new Book { Id = 3, Title = "Brave New World", Author = "Aldous Huxley", Year = 1932 },
+            new Book { Id = 4, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Year = 1925 },
+            new Book { Id = 5, Title = "Moby Dick", Author = "Herman Melville", Year = 1851 },
+            new Book { Id = 6, Title = "Pride and Prejudice", Author = "Jane Austen", Year = 1813 },
+            new Book { Id = 7, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Year = 1951 },
+            new Book { Id = 8, Title = "The Hobbit", Author = "J.R.R. Tolkien", Year = 1937 },
+            new Book { Id = 9, Title = "Fahrenheit 451", Author = "Ray Bradbury", Year = 1953 },
+            new Book { Id = 10, Title = "Animal Farm", Author = "George Orwell", Year = 1945 }
         };
     }
 }
